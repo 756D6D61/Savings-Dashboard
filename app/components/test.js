1b6621aba3bc2178f20cdc5ea83e5a3e219bd554
756D6D61 <756D6D61@gmail.com>
2019-08-24 11:33:17 +0100

    File structure. 📂

---
+++ app/components/test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Test extends React.Component{
+    render(){
+        return(
+            <div>
+                Hello test
+                </div>
+            
+        )
+    }
+}
+
+export default Test;
--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+import Test from './components/test'
 class App extends React.Component{
     render(){
         return(
-            <div>Hello World</div>
+            <div>
+                Hello World
+                <Test />
+                </div>
+            
         )
     }
 }
