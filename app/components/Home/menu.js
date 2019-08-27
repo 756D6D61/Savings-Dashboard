17011dc0c5d75ca3c38ab225b8edfcdfbeeea6ca
756D6D61 <756D6D61@gmail.com>
2019-08-27 12:45:05 +0100

    Header complete.

--- app/components/Home/menu.js
+++ app/components/Home/menu.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
-import { Container } from 'reactstrap'
-import Border from '../Images/border.svg'
-
+import { Container, Col, Row } from 'reactstrap'
+import HomeIcon from '@material-ui/icons/Home'
+import AddBoxIcon from '@material-ui/icons/AddBox'
+import Head from '../Images/head.svg'
 class Menu extends Component{
     render(){
         return(
+            <div className="menu-back">
             <Container>
-                <div>
-                menu
+                <Row>
+                    <Col md={1}><HomeIcon/></Col>
+                    <Col md={1}> <AddBoxIcon /></Col>
+                    <Col md={8}></Col>
+                    <Col md={2} ><Head  height='40px'/>Welcome, user</Col>
                 <div className="menu"></div>
-                </div>               
+                </Row>  
             </Container>
+            </div>      
+           
         )
     }
 }
