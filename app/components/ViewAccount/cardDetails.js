8f9635ba1206ee05a6402b2e90b1ad101624b830
756D6D61 <756D6D61@gmail.com>
2019-08-27 13:53:00 +0100

    Removed routing from card in view account.

--- app/components/ViewAccount/cardDetails.js
+++ app/components/ViewAccount/cardDetails.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
-import { Link } from 'react-router-dom'
 
 class Accounts extends Component{
     render(){
         return(
             <Container>
                 <Row>
-                    <Col><Link to="ViewAccount"><div className="cardHome1"></div></Link></Col>
+                    <Col><div className="cardHome1"></div></Col>
                 </Row>
             </Container>
         )
