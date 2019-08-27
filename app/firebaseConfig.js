29cdc0041a958d687111756e36d0b7ae1bf61bfb
756D6D61 <756D6D61@gmail.com>
2019-08-27 16:33:26 +0100

    firebase

---
+++ app/firebaseConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyD_R0q0JASTSmrWo2ZwVVH9M8sF2i82cjY",
+    authDomain: "auth-react-bb6f8.firebaseapp.com",
+    databaseURL: "https://auth-react-bb6f8.firebaseio.com",
+    projectId: "auth-react-bb6f8",
+    storageBucket: "",
+    messagingSenderId: "796138396188",
+    appId: "1:796138396188:web:cc374a86af612fa3"
+}
+
+export default config;
