f22bbbab076b553370faa6e09b42cc6013427297
756D6D61 <756D6D61@gmail.com>
2019-08-27 10:24:59 +0100

    Sign up complete

--- app/components/signUp.js
+++ app/components/signUp.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import Form from './SignUp/form'
+import People from './Images/people.svg'
+import { Container, Row, Col } from 'reactstrap'
 
 class SignUp extends Component{
     render(){
         return(
             <div className="auth">
-                <h1>Sign Up</h1>
-                <Form />
+                <Container>
+                        <h1 className="logIn">Sign Up</h1>
+                        <hr />
+                        <Row>
+                        <Col md={2}></Col>
+                        <Col md={8}><Form /></Col>
+                        <Col md={2}></Col>
+                        </Row>
+                        <div className="image">
+                            <People />
+                        </div>
+                </Container>
             </div>
             
         )
