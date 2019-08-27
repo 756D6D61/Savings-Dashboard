f4a05a1e34eb2f67a557dea295bbd5e70b9bb5c6
756D6D61 <756D6D61@gmail.com>
2019-08-27 13:32:58 +0100

    Routing added to cards.

--- app/components/Home/accounts.js
+++ app/components/Home/accounts.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 class Accounts extends Component{
     render(){
         return(
             <Container>
                 <Row>
-                    <Col><div className="cardHome1"></div></Col>
-                    <Col><div className="cardHome2"></div></Col>
-                    <Col><div className="cardHome3"></div></Col>
+                    <Col><Link to="ViewAccount"><div className="cardHome1"></div></Link></Col>
+                    <Col><Link to="ViewAccount"><div className="cardHome2"></div></Link></Col>
+                    <Col><Link to="ViewAccount"><div className="cardHome3"></div></Link></Col>
                 </Row>
             </Container>
         )
