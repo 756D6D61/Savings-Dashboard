74b91fcb3f966411ea63e22821f27b01723892f2
756D6D61 <756D6D61@gmail.com>
2019-09-09 18:45:37 +0100

    Testing posting to API via add form.

--- app/components/NewAccount/form.js
+++ app/components/NewAccount/form.js
@@ -1,31 +1,72 @@
 import React, { Component } from 'react';
 import { Form, Label, FormGroup, Input, Button } from 'reactstrap'
+import axios from 'axios';
 
 class NewAccForm extends Component{
+    constructor() {
+        super();
+        this.state = {
+            formFields: {
+                accountName:'',
+                accountType:'',
+                interest: 0, 
+                savingsGoals: 0
+            }
+        }
+    }
     render(){
         return(
             <Form>
                 <FormGroup>
                     <Label>Account Name</Label>
-                    <Input />
+                    <Input 
+                        name="accountName" onChange={(e) => this.inputChangeHandler.call(this, e)} 
+                        value={this.state.formFields.accountName}
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Label>Account Type</Label>
-                    <Input />
+                    <Input
+                        name="accountType" onChange={(e) => this.inputChangeHandler.call(this, e)} 
+                        value={this.state.formFields.accountName}
+                     />
                 </FormGroup>
                 <FormGroup>
                     <Label>Interest Earned</Label>
-                    <Input />
+                    <Input 
+                        name="interest" onChange={(e) => this.inputChangeHandler.call(this, e)} 
+                        value={this.state.formFields.accountName}
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Label>Savings Goal </Label>
-                    <Input />
+                    <Input 
+                        name="savingsGoals" onChange={(e) => this.inputChangeHandler.call(this, e)} 
+                        value={this.state.formFields.accountName}
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Button>Submit</Button>
                 </FormGroup>
             </Form>
         )
+    }
+    inputChangeHandler(e) {
+        let formFields = {...this.state.formFields}
+        formFields[e.target.name] = e.target.value;
+        this.setState({
+            formFields
+        })
+    }
+
+    formHandler(formFields) {
+        axios.post('https://o39ren2sp5.execute-api.us-west-1.amazonaws.com/latest/api/contacts', formFields)
+            .then(function(response) {
+                console.log(response)
+            })
+            .catch(function(error) {
+                console.log(error)
+            })
     }
 }
 
