79a63c6b9289bc8538f39a890be1af0a2a6ebcaf
756D6D61 <756D6D61@gmail.com>
2019-09-28 23:42:54 +0100

    Monthly savings

--- app/components/NewHome/MonthlySavings.js
+++ app/components/NewHome/MonthlySavings.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import { Row, Col } from 'reactstrap'
 
 const Content = styled.div`
     height: 75.5vh;
@@ -11,12 +12,83 @@ const Content = styled.div`
     margin-top: 15px;
     margin-left:-100px;
 `
+const Month = styled.div`
+    height: 6vh;
+    width: 22vw;
+    background:#FFF;
+    color:#5202FA;
+    border-radius:10px;
+    margin-left:1.5vw;
+    margin-bottom: 10px;
+    padding:10px;
+    font-weight:200;
+`
+const Title = styled.p`
+    font-family: Helvetica Neue;
+    font-size: 30px;
+    text-align:center;
+    font-weight:500;
+`
 
 class MonthlySavings extends Component{
     render(){
         return(
             <Content>
-                monthly savings
+                <Title>Monthly Savings</Title>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">January</Col>
+                        <Col md={4} className="text-md-right">£1,020</Col>
+                    </Row>
+                </Month>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">February</Col>
+                        <Col md={4} className="text-md-right">£3,129</Col>
+                    </Row>
+                </Month>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">March</Col>
+                        <Col md={4} className="text-md-right">£2,009</Col>
+                    </Row>
+                </Month>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">April</Col>
+                        <Col md={4} className="text-md-right">£3,001</Col>
+                    </Row>
+                </Month>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">May</Col>
+                        <Col md={4} className="text-md-right">£1,100</Col>
+                    </Row>
+                </Month>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">June</Col>
+                        <Col md={4} className="text-md-right">£2,222</Col>
+                    </Row>
+                </Month>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">July</Col>
+                        <Col md={4} className="text-md-right">£1,010</Col>
+                    </Row>
+                </Month>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">August</Col>
+                        <Col md={4} className="text-md-right">£4,201</Col>
+                    </Row>
+                </Month>
+                <Month>
+                    <Row>
+                        <Col md={8} className="text-md-left">September</Col>
+                        <Col md={4} className="text-md-right">£1,204</Col>
+                    </Row>
+                </Month>
             </Content>
         )
     }
