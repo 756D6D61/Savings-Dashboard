6cb1f3f2147fbd7fb3d9b53be40f3e2c808f9ccd
756D6D61 <756D6D61@gmail.com>
2019-09-28 14:49:48 +0100

    monthly savings

--- app/components/NewHome/MonthlySavings.js
+++ app/components/NewHome/MonthlySavings.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Content = styled.div`
+    height: 75.5vh;
+    width: 25vw;
+    background:#F7F7F7;
+    color:#5202FA;
+    border-radius:20px;
+    padding: 5px;
+    margin-top: 15px;
+    margin-left:-100px;
+`
 
 class MonthlySavings extends Component{
     render(){
         return(
-            <div>
+            <Content>
                 monthly savings
-            </div>
+            </Content>
         )
     }
 }
