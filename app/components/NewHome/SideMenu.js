c8b2d01e3f3d470d33e0ba29b5a2d27f93dbbc5f
756D6D61 <756D6D61@gmail.com>
2019-09-28 14:53:53 +0100

    Modal change

--- app/components/NewHome/SideMenu.js
+++ app/components/NewHome/SideMenu.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import HomeIcon from '@material-ui/icons/Home';
 import AccountIcon from '@material-ui/icons/Dashboard';
 import { Link } from 'react-router-dom'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const Menu = styled.div`
     height: 100vh;
@@ -58,35 +57,13 @@ const StyledLink = styled(
     color: white;
   `;
 class SideMenu extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          modal: false
-        };
     
-        this.toggle = this.toggle.bind(this);
-      }
-    
-      toggle() {
-        this.setState(prevState => ({
-          modal: !prevState.modal
-        }));
-      }
     render(){
         return(
             <Menu>
                 <br />
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-                <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </ModalBody>
-                <ModalFooter>
-                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                </ModalFooter>
-                </Modal>
-                <Image onClick={this.toggle}/>
+                
+                <Image />
                 <UserName>User Name</UserName>
                 <HR />
                 <Links>
--- app/components/NewHome/Welcome.js
+++ app/components/NewHome/Welcome.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Row, Col } from 'reactstrap'
+import { Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import styled from 'styled-components'
 import Person from '@material-ui/icons/Person';
 
@@ -20,6 +20,20 @@ const Icon = styled.p`
 `
 
 class Welcome extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          modal: false
+        };
+    
+        this.toggle = this.toggle.bind(this);
+      }
+    
+      toggle() {
+        this.setState(prevState => ({
+          modal: !prevState.modal
+        }));
+      }
     render(){
         return(
             <Row>
@@ -27,8 +41,18 @@ class Welcome extends Component{
                     <UserName>Welcome, User Name</UserName>
                 </Col>
                 <Col md={1}>
-                    <Icon><Person style={{fontSize: '130%'}}/></Icon>
+                    <Icon><Person style={{fontSize: '130%'}} onClick={this.toggle}/></Icon>
                 </Col>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+                <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                </ModalFooter>
+                </Modal>
             </Row>
         )
     }
