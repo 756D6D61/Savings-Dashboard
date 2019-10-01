48cba9c32ec65ca14c07f4452f105b92636fd788
756D6D61 <756D6D61@gmail.com>
2019-10-01 21:24:13 +0100

    logic for showing component

--- app/components/NewAccountPage/SideAccounts.js
+++ app/components/NewAccountPage/SideAccounts.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
-
+import ViewAccountDetails from './ViewAccountDetails'
 class SideAccounts extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            childVisible: true
+        }
+    }
+   onClick() {
+       this.setState({
+           childVisible: !this.state.childVisible
+       })
+   }
     render(){
         return(
             <div>
                 side accounts - make scrollable
+                <button onClick={() => this.onClick()}>test</button>
+                {this.state.childVisible ? (
+                    <ViewAccountDetails />
+                ):null}
             </div>
         )
     }
