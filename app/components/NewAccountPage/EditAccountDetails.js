05a936f16408d7994f3c7ffdf0712c527c3edbfc
756D6D61 <756D6D61@gmail.com>
2019-10-04 21:03:09 +0100

    form outline - edit

--- app/components/NewAccountPage/EditAccountDetails.js
+++ app/components/NewAccountPage/EditAccountDetails.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
+import { Row, Col } from 'reactstrap'
 
 class EditAccount extends Component{
     render(){
         return(
             <div>
-                edit account details
+                <form>
+                    <Row>
+                        <Col>
+                            <label>
+                                Name:
+                            </label>
+                            <input type="text" name="name" />
+                        </Col>    
+                    </Row>
+                    <Row>
+                        <Col>
+                            <label>
+                                Name:
+                            </label>
+                            <input type="text" name="name" />
+                        </Col>    
+                    </Row>
+                    <Row>
+                        <Col>
+                            <label>
+                                Name:
+                            </label>
+                            <input type="text" name="name" />
+                        </Col>    
+                    </Row>
+                    <Row>
+                        <Col>
+                            <label>
+                                Name:
+                            </label>
+                            <input type="text" name="name" />
+                        </Col>    
+                    </Row>
+                    
+                    <input type="submit" value="Submit" />
+                    
+                </form>
             </div>
         )
     }
