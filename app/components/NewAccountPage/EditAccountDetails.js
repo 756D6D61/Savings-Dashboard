8bcc7cda0fb9adf99d9a83413671768d9478bfe5
756D6D61 <756D6D61@gmail.com>
2019-10-04 21:52:28 +0100

    Edit form styling

--- app/components/NewAccountPage/EditAccountDetails.js
+++ app/components/NewAccountPage/EditAccountDetails.js
@@ -1,47 +1,96 @@
 import React, { Component } from 'react'
 import { Row, Col } from 'reactstrap'
+import styled from 'styled-components'
+
+const Form = styled.form`
+    margin-top:10%;
+`
+const Label = styled.label`
+    margin-left:10%;
+    font-family: Helvetica Neue;
+    font-size: 30px;
+    text-align:center;
+    font-weight:500;
+    color: #5202FA;
+`
+const Input = styled.input`
+    margin-left:10%;
+    width:50vw;
+    height: 7vh;
+    border-radius: 10px;
+    border: transparent;
+    color: #5202FA;
+    background: #D5D8D0;
+    margin-bottom: 30px;
+    padding: 10px;
+    font-weight: 200;
+    ::placeholder,
+    ::-webkit-input-placeholder {
+        color: #5202FA;
+        font-size:25px;
+        padding:5px;
+  }
+`
+const Button = styled.button`
+    background:#D5D8D0;
+    width:100px;
+    height:50px;
+    color: #5202FA;
+    border-radius: 10px;
+    border: transparent;
+    font-weight:500;
+    font-size:22px;
+    margin-left:50%;
+`
+
 
 class EditAccount extends Component{
     render(){
         return(
             <div>
-                <form>
+                <Form>
                     <Row>
-                        <Col>
-                            <label>
-                                Name:
-                            </label>
-                            <input type="text" name="name" />
+                        <Col md={2}>
+                            <Label>
+                                Name
+                            </Label>
+                        </Col>  
+                        <Col md={10}>
+                            <Input type="text" name="name" placeholder="HSBC Super Saver"/>
                         </Col>    
                     </Row>
                     <Row>
-                        <Col>
-                            <label>
-                                Name:
-                            </label>
-                            <input type="text" name="name" />
+                        <Col md={2}>
+                            <Label>
+                                Type
+                            </Label>
+                        </Col>
+                        <Col md={10}>
+                            <Input type="text" name="name" placeholder="General"/>
                         </Col>    
                     </Row>
                     <Row>
-                        <Col>
-                            <label>
-                                Name:
-                            </label>
-                            <input type="text" name="name" />
+                        <Col md={2}>
+                            <Label>
+                                Goals
+                            </Label>
+                        </Col>
+                        <Col md={10}>
+                            <Input type="text" name="name" placeholder="£10,201"/>
                         </Col>    
                     </Row>
                     <Row>
-                        <Col>
-                            <label>
-                                Name:
-                            </label>
-                            <input type="text" name="name" />
+                        <Col md={2}>
+                            <Label>
+                                Interest
+                            </Label>
+                        </Col>
+                        <Col md={10}>
+                            <Input type="text" name="name" placeholder="0.05%"/>
                         </Col>    
                     </Row>
-                    
-                    <input type="submit" value="Submit" />
-                    
-                </form>
+                    <Button>Submit</Button>
+                </Form>
             </div>
         )
     }
