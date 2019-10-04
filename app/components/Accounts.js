ca573b116e57a3ef881fbcfefae7701be1e3449c
756D6D61 <756D6D61@gmail.com>
2019-10-04 22:03:42 +0100

    Add icon on accounts page

--- app/components/Accounts.js
+++ app/components/Accounts.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react'
 import TopMenu from './NewAccountPage/TopMenu'
 import AccountContainer from './NewAccountPage/AccountContainer'
 import { Row, Col } from 'reactstrap'
+import styled from 'styled-components'
+import NewIcon from './Images/add.svg'
+import { Link } from 'react-router-dom'
+
+const AddIcon = styled.div`
+    z-index:999;
+    margin-left:95%;
+    margin-top:-7.5%;
+    position: static; 
+`
 
 class Accounts extends Component{
     render(){
@@ -16,6 +26,9 @@ class Accounts extends Component{
                     <Col>
                         <AccountContainer />
                     </Col>
+                    <AddIcon>
+                    <Link to="/AddAccounts"><NewIcon /></Link>
+                </AddIcon>
                 </Row>
             </div>
         )
